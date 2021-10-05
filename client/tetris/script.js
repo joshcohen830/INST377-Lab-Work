6fdc7e5b87b767314e98859f37f674e68ca0b4da
joshcohen830 <JCOHEN43@UMD.EDU>
2021-10-05 14:26:40 -0400

    Fix rotation issue

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -4,6 +4,8 @@ document.addEventListener('DOMContentLoaded', () => {
   const scoreDisplay = document.querySelector('#score')
   const startBtn = document.querySelector('#start-button')
   const width = 10
+  let nextRandom = 0
+  let timerId
 
   const lTetromino = [
     [1, width+1, width*2+1, 2],
@@ -86,10 +88,12 @@ document.addEventListener('DOMContentLoaded', () => {
   function freeze() {
     if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
       current.forEach(index => squares[currentPosition + index].classList.add('taken'))
-      random = Math.floor(Math.random() * theTretrominoes.length)
+      random = nextRandom
+      nextRandom = Math.floor(Math.random() * theTretrominoes.length)
       current = theTretrominoes[random][currentRotation]
       currentPosition = 4
       draw()
+      displayShape()
     }
   }
 
@@ -124,12 +128,49 @@ document.addEventListener('DOMContentLoaded', () => {
   function rotate() {
     undraw()
     currentRotation ++
-    if(currentRotation === current.lenght) {
+    if(currentRotation === current.length) {
       currentRotation = 0
     }
     current = theTretrominoes[random][currentRotation]
     draw()
   }
+
+  const displaySquares = document.querySelectorAll('.mini-grid div')
+  const displayWidth = 4
+  let displayIndex = 0
+
+
+  const upNextTetrominoes = [
+    [1, displayWidth+1, displayWidth*2+1, 2],
+    [0, displayWidth, displayWidth+1, displayWidth*2+1],
+    [1, displayWidth, displayWidth+1, displayWidth+2], 
+    [0, 1, displayWidth, displayWidth+1], 
+    [1, displayWidth+1, displayWidth*2+1, displayWidth*3+1] 
+  ]
+
+  function displayShape() {
+    displaySquares.forEach(square => {
+      square.classList.remove('tetromino')
+    })
+    upNextTetrominoes[nextRandom].forEach( index => {
+      displaySquares[displayIndex + index].classList.add('tetromino')
+    })
+  }
+
+  startBtn.addEventListener('click', () => {
+    if (timerId) {
+      clearInterval(timerId)
+      timerId = null
+    } else {
+      draw()
+      timerId = setInterval(moveDown, 1000)
+      nextRandom = Math.floor(Math.random()*theTretrominoes.length)
+      displayShape()
+    }
+  })
+
+
+
 
 
 })
